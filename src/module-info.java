151910739ca83fe84b07f92c5c5ed6af9ada0ba0
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-11 20:31:40 -0300

    initial commit

---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module module_info.java {
+	requires java.desktop;
+}
---
+++ src/pacoteRevisao/Classe.java
@@ -0,0 +1,9 @@
+package pacoteRevisao;
+
+public class Classe {
+
+	public static void main(String[] args) {
+		System.out.print("Eu sou lindo");
+	}
+
+}
