9071ec0b9576913ed198732300b4b0b62ef8e066
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-17 09:42:39 -0300

    feat: adding dialog of message error and sucess

---
+++ src/pacoteRevisao/Dialog.java
@@ -0,0 +1,69 @@
+package pacoteRevisao;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Dialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			Dialog dialog = new Dialog("");
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 * @param string 
+	 */
+	public Dialog(String message) {
+		setLocationRelativeTo(null);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		setTitle("Erro!");
+		{
+			JLabel feedbackMessage = new JLabel(message);
+			feedbackMessage.setFont(new Font("Ubuntu", Font.PLAIN, 24));
+			feedbackMessage.setBounds(48, 79, 340, 62);
+			contentPanel.add(feedbackMessage);
+		}
+		
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						Dialog.this.dispose();
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+	}
+}
