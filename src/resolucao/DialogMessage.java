f52999ce98f8c838643aa2a78c480b3f451ee6c1
Marcus Sandi <tradedomarcus@gmail.com>
2023-04-04 08:41:20 -0300

    feat: dialog message when to warn white spaces

---
+++ src/resolucao/DialogMessage.java
@@ -0,0 +1,70 @@
+package resolucao;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class DialogMessage extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			DialogMessage dialog = new DialogMessage("");
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 * 
+	 */
+	public DialogMessage(String message) {
+		setLocationRelativeTo(null);
+		setBounds(100, 100, 341, 181);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		
+		JLabel messageText = new JLabel(message);
+		messageText.setFont(new Font("Ubuntu", Font.BOLD, 22));
+		messageText.setBounds(10, 29, 305, 69);
+		contentPanel.add(messageText);
+		
+		setTitle("Aviso!");
+		setVisible(true);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						DialogMessage.this.dispose();
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			
+		}
+	}
+}
