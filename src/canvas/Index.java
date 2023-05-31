17fed2f56b9d05270520be5f9ba3590a6558bd9b
Marcus Sandi <tradedomarcus@gmail.com>
2023-05-31 10:04:26 -0300

    feat: adding menu with each options (client, adress and phone)

--- src/canvas/Index.java
+++ src/canvas/Index.java
@@ -5,6 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Index extends JFrame {
 
@@ -32,10 +36,41 @@ public class Index extends JFrame {
 	public Index() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnClient = new JMenu("Cliente");
+		menuBar.add(mnClient);
+		
+		JMenuItem subRegisterClient = new JMenuItem("Cadastrar");
+		mnClient.add(subRegisterClient);
+		
+		JMenuItem subListClient = new JMenuItem("Listagem");
+		mnClient.add(subListClient);
+		
+		JMenu mnAdress = new JMenu("Endereço");
+		menuBar.add(mnAdress);
+		
+		JMenuItem subRegisterAdress = new JMenuItem("Cadastrar");
+		mnAdress.add(subRegisterAdress);
+		
+		JMenuItem subListAdress = new JMenuItem("Listagem");
+		mnAdress.add(subListAdress);
+		
+		JMenu mnPhone = new JMenu("Telefone");
+		menuBar.add(mnPhone);
+		
+		JMenuItem subRegisterPhone = new JMenuItem("Cadastrar");
+		mnPhone.add(subRegisterPhone);
+		
+		JMenuItem subListPhone = new JMenuItem("Listagem");
+		mnPhone.add(subListPhone);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 	}
 
 }
